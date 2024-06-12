f374c224ddfbff23fc311cfd9963a93b4835ea26
Lulu Nwenyi <oluchinwenyi@gmail.com>
2024-06-12 23:25:57 +0100

    fix: added certifi for mongodb

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,6 @@
 # imports #
 # ------- #
-import os
+import os, certifi
 from flask import Flask
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
@@ -13,7 +13,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 uri = os.environ.get("MONGO_URI")
-client = MongoClient(uri, server_api=ServerApi('1'), tls=True)
+client = MongoClient(uri, server_api=ServerApi('1'), tls=True, tlsCAFile=certifi.where())
 
 app.config["JWT_SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
