2da6571398a76f9d4d819e84f5beb77ab64a364c
Lulu Nwenyi <oluchinwenyi@gmail.com>
2024-07-21 19:39:28 +0100

    fix: returned lists in profile

--- api/profile/routes.py
+++ api/profile/routes.py
@@ -2,7 +2,7 @@
 # ------- # 
 from flask import Blueprint, jsonify
 from bson import ObjectId
-from api.collections import user_collection, wish_collection
+from api.collections import user_collection, wish_collection, list_collection
 
 profile = Blueprint('profile', __name__)
 
@@ -35,10 +35,27 @@ def user_public_profile(username):
                 
             total_wishes = len(wishes)
             
+            user_lists = list_collection.find({"user_id": ObjectId(user_id)})
+        
+            wishlists = []
+            for each_list in user_lists:
+                list_data = {}
+                list_data["id"] = str(each_list['_id'])
+                list_data["user_id"] = str(each_list['user_id'])
+                list_data["title"] = str(each_list['title'])
+                list_data["description"] = str(each_list['description'])
+                list_data["category"] = str(each_list['category'])
+                list_data["expiry_date"] = str(each_list['expiry_date'])
+                list_data["display_hex_code"] = str(each_list['display_hex_code'])
+                list_data["wishes"] = str(each_list['wishes'])
+
+                wishlists.append(list_data)
+            
             response = {
                 "name": user['name'],
                 "username": user['username'],
                 "public_id": user['public_id'],
+                "lists": wishlists,
                 "total_claims": total_claims,
                 "total_wishes": total_wishes
             }
